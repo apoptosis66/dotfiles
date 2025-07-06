69d1653ebb7cd84dc83ca4b6a9ba2ba4b1c37c94
apoptosis66 <jacob.champlin@gmail.com>
2025-07-06 15:53:12 -0500

    themes were installing even if not needed.

--- install/theme.sh
+++ install/theme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Use dark mode for QT apps too (like VLC and kdenlive)
-yay -S --noconfirm kvantum-qt5
+yay  -S --noconfirm --needed kvantum-qt5
 
 # Prefer dark mode everything
-yay -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
+yay  -S --noconfirm --needed gnome-themes-extra # Adds Adwaita-dark theme
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
