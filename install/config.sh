bfb8ea144f9e5e20b25e2d2bfe6f7317f6727cd1
apoptosis66 <jacob.champlin@gmail.com>
2025-07-07 21:15:10 -0500

    tokyo-night theme update

--- install/config.sh
+++ install/config.sh
@@ -12,12 +12,3 @@ if [ -f ~/.bashrc ]; then
 fi
 cp default/bashrc ~/.bashrc
 source ~/.bashrc
-
-# Login directly as user, rely on disk encryption + hyprlock for security
-# Commenting out for now because I haven't gone full disk encryption
-#sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-#sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf >/dev/null <<EOF
-#[Service]
-#ExecStart=
-#ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I \$TERM
-#EOF
