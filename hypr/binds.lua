7da7cbb7aa3a78dc1244a8810367d0478b3bc7c0
apoptosis66 <jacob.champlin@gmail.com>
2026-08-11 16:39:41 -0500

    noctalia launcher

--- hypr/binds.lua
+++ hypr/binds.lua
@@ -29,7 +29,7 @@ hl.bind("SUPER + Y", hl.dsp.exec_cmd(browser .. " https://youtube.com/"))
 
 -- Hyprland
 -- hl.bind("SUPER + SPACE", hl.dsp.exec_cmd('flock --nonblock /tmp/.rofi.lock -c "rofi -show drun -sort"'))
-hl.bind("SUPER + SPACE", hl.dsp.exec_cmd("uv run " .. scripts .. "control-panel.py"))
+hl.bind("SUPER + SPACE", hl.dsp.exec_cmd("noctalia msg panel-toggle launcher"))
 hl.bind("SUPER + SHIFT + SPACE", hl.dsp.exec_cmd("pkill waybar && hyprctl eval 'hl.exec_cmd(\"waybar\")'"))
 hl.bind("SUPER + CTRL + SPACE", hl.dsp.exec_cmd(scripts .. "theme-menu.sh"))
 hl.bind("SUPER + Q", hl.dsp.window.close())
